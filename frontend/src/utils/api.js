bdbd3def7cca4a05ef491703728694aad18d7603
Shridul Sethi <123456+shridulsethi@users.noreply.github.com>
2026-02-02 04:02:16 +0530

    api.js fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,5 @@
-export const API_BASE = "http://localhost:8080";
+export const API_BASE =
+    process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 export const authHeader = {
   Authorization: "Basic " + btoa("admin:admin123"),
